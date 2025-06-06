bb59e47a2c5ae33305a209c27200d67b70973fb8
MatheusGrupp <matheusgrupp.rosa@gmail.com>
2025-06-06 15:12:55 -0300

    rollback

--- projeto-roupa-infantil/products/app/models.py
+++
@@ -1,5 +0,0 @@
-class Product(db.Model):
-    __tablename__ = 'products'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price = db.Column(db.Float, nullable=False)
--- projeto-roupa-infantil/products/run.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-
-if __name__ == "__main__":
-    # Garante que as tabelas do banco de dados sejam criadas
-    with app.app_context():
-        db.create_all()
-    # Executa a aplicação Flask na porta 5001, acessível externamente
-    app.run(host="0.0.0.0", port=5001)
